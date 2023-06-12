0989b5e98e36d6e8c7a01894d9d2ccd6684a2780
Ryan Dunn <grenderk@live.com>
2023-06-12 13:34:22 -0400

    adds led lightup for capslock

--- keyboards/gmmk/pro/rev1/ansi/keymaps/grenderk/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/grenderk/keymap.c
@@ -66,3 +66,64 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
 };
 #endif
+
+#ifdef RGB_MATRIX_ENABLE
+static void set_rgb_caps_leds_on(void);
+static void set_rgb_caps_leds_off(void);
+
+bool rgb_matrix_indicators_user(void) {
+    if (host_keyboard_led_state().caps_lock) {
+        set_rgb_caps_leds_on();
+    } else {
+      if (rgb_matrix_get_flags() == LED_FLAG_NONE){
+        set_rgb_caps_leds_off();
+      }
+    }
+
+    return false;
+}
+
+static void set_rgb_caps_leds_on(void) {
+        rgb_matrix_set_color(0, 255, 0, 0);       //Escape Key
+        rgb_matrix_set_color(3, 255, 0, 0);       //capslock key
+        rgb_matrix_set_color(5, 255, 0, 0);       //Left CTRL key
+        rgb_matrix_set_color(67, 255, 0, 0);      //Left LED 01
+        rgb_matrix_set_color(68, 255, 0, 0);      //Right LED 12
+        rgb_matrix_set_color(70, 255, 127, 0);    //Left LED 02
+        rgb_matrix_set_color(71, 255, 127, 0);    //Right LED 13
+        rgb_matrix_set_color(73, 255, 255, 0);    //Left LED 03
+        rgb_matrix_set_color(74, 255, 255, 0);    //Right LED 14
+        rgb_matrix_set_color(76, 0, 255, 0);      //Left LED 04
+        rgb_matrix_set_color(77, 0, 255, 0);      //Right LED 15
+        rgb_matrix_set_color(80, 0, 0, 255);      //Left LED 05
+        rgb_matrix_set_color(81, 0, 0, 255);      //Right LED 16
+        rgb_matrix_set_color(83, 46, 43, 95);     //Left LED 06
+        rgb_matrix_set_color(84, 46, 43, 95);     //Right LED 17
+        rgb_matrix_set_color(87, 139, 0, 255);    //Left LED 07
+        rgb_matrix_set_color(88, 139, 0, 255);    //Right LED 18
+        rgb_matrix_set_color(91, 255, 255, 255);  //Left LED 08
+        rgb_matrix_set_color(92, 255, 255, 255);  //Right LED 19
+}
+
+static void set_rgb_caps_leds_off(void) {
+        rgb_matrix_set_color(0,  0, 0, 0); //Escape Key
+        rgb_matrix_set_color(3,  0, 0, 0); //capslock key
+        rgb_matrix_set_color(5,  0, 0, 0); //Left CTRL key
+        rgb_matrix_set_color(67, 0, 0, 0); //Left LED 01
+        rgb_matrix_set_color(68, 0, 0, 0); //Right LED 12
+        rgb_matrix_set_color(70, 0, 0, 0); //Left LED 02
+        rgb_matrix_set_color(71, 0, 0, 0); //Right LED 13
+        rgb_matrix_set_color(73, 0, 0, 0); //Left LED 03
+        rgb_matrix_set_color(74, 0, 0, 0); //Right LED 14
+        rgb_matrix_set_color(76, 0, 0, 0); //Left LED 04
+        rgb_matrix_set_color(77, 0, 0, 0); //Right LED 15
+        rgb_matrix_set_color(80, 0, 0, 0); //Left LED 05
+        rgb_matrix_set_color(81, 0, 0, 0); //Right LED 16
+        rgb_matrix_set_color(83, 0, 0, 0); //Left LED 06
+        rgb_matrix_set_color(84, 0, 0, 0); //Right LED 17
+        rgb_matrix_set_color(87, 0, 0, 0); //Left LED 07
+        rgb_matrix_set_color(88, 0, 0, 0); //Right LED 18
+        rgb_matrix_set_color(91, 0, 0, 0); //Left LED 08
+        rgb_matrix_set_color(92, 0, 0, 0); //Right LED 19
+}
+#endif
